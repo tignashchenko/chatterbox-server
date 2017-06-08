cf530625a44229777a7d5e6f660ad92ce2841604
Taras Ignashchenko <tignashchenko@gmail.com>
2017-06-08 08:20:39 -0700

    Pass 14 Mocha tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,46 +11,47 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+
 var responseBody = {
   results: []
 };
 
-var headers = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-  'content-type': 'text/plain'
-};
-
 var requestHandler = function(request, response) {
+
+  var headers = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10, // Seconds.
+    'content-type': 'text/plain'
+  };
 
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  var successCode = 200;
-  var postCode = 201;
-  var errorCode = 404;
-
   if (request.method === 'POST') {
+    var body = '';
     request.on('data', function(chunk) {
       responseBody.results.push(JSON.parse(chunk.toString()));
     });
 
     request.on('end', function(message) {
-      response.writeHead(postCode, headers);
+      response.writeHead(201, headers);
+      //console.log(response.statusCode);
       response.end(JSON.stringify(responseBody));
     });
   } else if (request.method === 'GET' && request.url === '/classes/messages') {
-    response.writeHead(successCode, headers);
+    response.writeHead(200, headers);
+    //console.log(response.statusCode);
     response.end(JSON.stringify(responseBody));
   } else if (request.method === 'OPTIONS') {
-    response.writeHead(successCode, headers);
+    response.writeHead(200, headers);
+    //console.log(response.statusCode);
     response.end(JSON.stringify(responseBody));
   } else {
-    response.statusCode = errorCode;
-    response.end();
+    response.writeHead(404, headers);
+    //console.log(response.statusCode);
+    response.end(JSON.stringify(responseBody));
   }
-  console.log(request.url);
 };
 
-module.exports= requestHandler;
+module.exports.requestHandler = requestHandler;
